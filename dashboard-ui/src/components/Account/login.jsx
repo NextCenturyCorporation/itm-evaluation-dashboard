2f003d1b126394d57353376fa5e2f75497e161cd
dereknop <dereknop@gmail.com>
2023-09-21 13:05:05 -0400

    added to .gitignore, fixed bug with login page
    Create tab wasn't doing anything when clicked. Used bootstrap built in Tabs component

--- dashboard-ui/src/components/Account/login.jsx
+++ dashboard-ui/src/components/Account/login.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {accountsPassword} from '../../services/accountsService';
-import {withRouter} from 'react-router-dom';
+import { accountsPassword } from '../../services/accountsService';
+import { withRouter } from 'react-router-dom';
 import $ from 'jquery';
-
 import brandImage from '../../img/logo.png';
-
+import { Tabs, Tab } from 'react-bootstrap'
 class LoginApp extends React.Component {
 
     constructor(props) {
@@ -23,7 +22,7 @@ class LoginApp extends React.Component {
         this.createAccount = this.createAccount.bind(this);
     }
 
-    createAccount = async() => {
+    createAccount = async () => {
         $("#create-account-feedback").removeClass("feedback-display");
         try {
             let results = await accountsPassword.createUser({
@@ -48,11 +47,12 @@ class LoginApp extends React.Component {
         }
     }
 
-    login = async() => {
+    login = async () => {
+        console.log("login hit")
         $("#sign-in-feedback").removeClass("feedback-display");
         try {
             let results;
-            if(this.state.userName.indexOf('@') > -1) {
+            if (this.state.userName.indexOf('@') > -1) {
                 results = await accountsPassword.login({
                     password: this.state.password,
                     user: {
@@ -73,10 +73,11 @@ class LoginApp extends React.Component {
         } catch (err) {
             $("#sign-in-feedback").addClass("feedback-display");
             this.setState({ error: err.message });
+            console.log(err.message)
         }
     }
 
-    resetPassword = async() => {
+    resetPassword = async () => {
         try {
             await accountsPassword.requestPasswordReset(this.state.forgotEmail);
 
@@ -88,28 +89,28 @@ class LoginApp extends React.Component {
         }
     }
 
-    onChangeUserName = ({target}) => {
-        this.setState({userName: target.value});
+    onChangeUserName = ({ target }) => {
+        this.setState({ userName: target.value });
     }
 
-    onChangePassword = ({target}) => {
-        this.setState({password: target.value});
+    onChangePassword = ({ target }) => {
+        this.setState({ password: target.value });
     }
 
-    onChangeCreateUserName = ({target}) => {
-        this.setState({createUserName: target.value});
+    onChangeCreateUserName = ({ target }) => {
+        this.setState({ createUserName: target.value });
     }
 
-    onChangeCreateEmail = ({target}) => {
-        this.setState({createEmail: target.value});
+    onChangeCreateEmail = ({ target }) => {
+        this.setState({ createEmail: target.value });
     }
 
-    onChangeCreatePassword = ({target}) => {
-        this.setState({createPassword: target.value});
+    onChangeCreatePassword = ({ target }) => {
+        this.setState({ createPassword: target.value });
     }
 
-    onChangeForgotEmail = ({target}) => {
-        this.setState({forgotEmail: target.value});
+    onChangeForgotEmail = ({ target }) => {
+        this.setState({ forgotEmail: target.value });
     }
 
     showSignIn = (evt) => {
@@ -128,20 +129,13 @@ class LoginApp extends React.Component {
                 <div className="row justify-content-center align-items-center h-100 center-container">
                     <div className="col col-md-4 login-container">
                         <div className="login-header-logo">
-                            <div className="d-flex justify-content-center"><img className="nav-brand" src={brandImage} alt=""/></div>
+                            <div className="d-flex justify-content-center"><img src={brandImage} alt="" /></div>
                             <div className="d-flex justify-content-center login-header-text">Dashboard</div>
                         </div>
-                        <div>
-                            <ul className="nav nav-tabs" id="myTab" role="tablist">
-                                <li className="nav-item">
-                                    <a className="nav-link" id="create-tab" data-toggle="tab" href="#create" role="tab" aria-controls="create" aria-selected="false">Create Account</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link active" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="true" onClick={this.showSignIn}>Sign In</a>
-                                </li>
-                            </ul>
-                            <div className="tab-content login-tab-content" id="myTabContent">
-                                <div className="tab-pane fade" id="create" role="tabpanel" aria-labelledby="create-tab">
+
+                        <Tabs className='p-1'>
+                            <Tab eventKey={0} title="Create Account">
+                                <div>
                                     <div className="sign-in-instructions">
                                         <div className="sign-in-header">Create Account</div>
                                         <div>Create an account to save, share and comment on queries.</div>
@@ -149,15 +143,15 @@ class LoginApp extends React.Component {
                                     <form>
                                         <div className="form-group">
                                             <div className="input-login-header">Email Address</div>
-                                            <input className="form-control form-control-lg" placeholder="Email" type="text" id="createEmail" value={this.state.createEmail} onChange={this.onChangeCreateEmail}/>
+                                            <input className="form-control form-control-lg" placeholder="Email" type="text" id="createEmail" value={this.state.createEmail} onChange={this.onChangeCreateEmail} />
                                         </div>
                                         <div className="form-group">
                                             <div className="input-login-header">Username</div>
-                                            <input className="form-control form-control-lg" placeholder="Username" type="text" id="createUserName" value={this.state.createUserName} onChange={this.onChangeCreateUserName}/>
+                                            <input className="form-control form-control-lg" placeholder="Username" type="text" id="createUserName" value={this.state.createUserName} onChange={this.onChangeCreateUserName} />
                                         </div>
                                         <div className="form-group">
                                             <div className="input-login-header">Password</div>
-                                            <input className="form-control form-control-lg" placeholder="Password" type="password" id="createPassword" value={this.state.createPassword} onChange={this.onChangeCreatePassword}/>
+                                            <input className="form-control form-control-lg" placeholder="Password" type="password" id="createPassword" value={this.state.createPassword} onChange={this.onChangeCreatePassword} />
                                         </div>
                                         <div className="form-group">
                                             <button className="btn btn-primary btn-lg btn-block" onClick={this.createAccount} type="button">Create Account</button>
@@ -167,7 +161,9 @@ class LoginApp extends React.Component {
                                         </div>
                                     </form>
                                 </div>
-                                <div className="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="login-tab">
+                            </Tab>
+                            <Tab eventKey={1} title="Sign In">
+                                <div >
                                     <div id="sign-in-pane">
                                         <div className="sign-in-instructions">
                                             <div className="sign-in-header">Sign in</div>
@@ -176,11 +172,11 @@ class LoginApp extends React.Component {
                                         <form>
                                             <div className="form-group">
                                                 <div className="input-login-header">Email Address or Username</div>
-                                                <input className="form-control form-control-lg" placeholder="Email / Username" type="text" id="userName" value={this.state.userName} onChange={this.onChangeUserName}/>
+                                                <input className="form-control form-control-lg" placeholder="Email / Username" type="text" id="userName" value={this.state.userName} onChange={this.onChangeUserName} />
                                             </div>
                                             <div className="form-group">
                                                 <div className="input-login-header">Password</div>
-                                                <input className="form-control form-control-lg" placeholder="Password" type="password" id="password" value={this.state.password} onChange={this.onChangePassword}/>
+                                                <input className="form-control form-control-lg" placeholder="Password" type="password" id="password" value={this.state.password} onChange={this.onChangePassword} />
                                             </div>
                                             <div className="form-group">
                                                 <div className="forgot-password">
@@ -204,7 +200,7 @@ class LoginApp extends React.Component {
                                             <form>
                                                 <div className="form-group">
                                                     <div className="input-login-header">Email Address</div>
-                                                    <input className="form-control form-control-lg" placeholder="Email" type="text" id="forgotEmail" value={this.state.forgotEmail} onChange={this.onChangeForgotEmail}/>
+                                                    <input className="form-control form-control-lg" placeholder="Email" type="text" id="forgotEmail" value={this.state.forgotEmail} onChange={this.onChangeForgotEmail} />
                                                 </div>
                                                 <div className="form-group">
                                                     <button className="btn btn-primary btn-lg btn-block" onClick={this.resetPassword} type="button">Send Reset Email</button>
@@ -222,8 +218,8 @@ class LoginApp extends React.Component {
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
+                            </Tab>
+                        </Tabs>
                     </div>
                 </div>
             </div>
