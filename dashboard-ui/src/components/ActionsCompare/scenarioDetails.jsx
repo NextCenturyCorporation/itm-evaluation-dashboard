0c9a8808fa20d8d83b6bcab9bf78093fcb7d0d71
dereknop <dereknop@gmail.com>
2023-10-04 16:38:23 -0400

    Replaced old query with new one, added more patient details.
    Adjusted to lookup scenario based on scenario prop being passed from parent component.

--- dashboard-ui/src/components/ActionsCompare/scenarioDetails.jsx
+++ dashboard-ui/src/components/ActionsCompare/scenarioDetails.jsx
@@ -4,13 +4,10 @@ import { nameMappings } from './decisionList';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const test_by_adm_and_scenario = gql`
-    query getTestByADMandScenario($scenarioID: ID, $admName: ID){
-        getTestByADMandScenario(scenarioID: $scenarioID, admName: $admName)
+const getScenarioQuery = gql`
+    query getScenario($scenarioId: ID) {
+        getScenario(scenarioId: $scenarioId) 
     }`;
-const myQuery = gql`query ExampleQuery {
-    getAllHistory
-  }`
 class ScenarioDetails extends React.Component {
 
     constructor(props) {
@@ -22,70 +19,78 @@ class ScenarioDetails extends React.Component {
 
     renderPatientInfo(patient) {
         return (
-            <ListGroup variant="flush">
-                <ListGroup.Item>Age: {patient.demographics.age}</ListGroup.Item>
-                <ListGroup.Item>Sex: {patient.demographics.sex}</ListGroup.Item>
-                <ListGroup.Item>Rank: {patient.demographics.rank}</ListGroup.Item>
-            </ListGroup>
-        )
+            <div className="row">
+                <div className="col-md-6">
+                    <h3>Demographic Information</h3>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>Age: {patient.demographics.age}</ListGroup.Item>
+                        <ListGroup.Item>Sex: {patient.demographics.sex}</ListGroup.Item>
+                        <ListGroup.Item>Rank: {patient.demographics.rank}</ListGroup.Item>
+                    </ListGroup>
+                </div>
+                <div className="col-md-6">
+                    <h3>Injuries</h3>
+                    <ListGroup variant="flush">
+                        {patient.injuries.map((injury, index) => (
+                            <ListGroup.Item key={index}>
+                                <strong>Name:</strong> {injury.name}<br />
+                                <strong>Location:</strong> {injury.location}<br />
+                                <strong>Severity:</strong> {injury.severity}
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                </div>
+            </div>
+        );
     }
+    
+    
 
     componentDidUpdate(prevProps) {
         if (this.props.selectedScenario !== prevProps.selectedScenario) {
-            if (this.props.selectedScenario === "Soartech") {
-                this.setState({ scenarioId: "soartech-september-demo-scenario-1" })
-            } else {
-                this.setState({scenarioId: "adept-september-demo-scenario-1"})
-            }
+            this.setState({ scenarioId: this.props.selectedScenario })
         }
     }
 
     render = () => {
         return (
             <Accordion>
-                { (this.props.selectedScenario && this.state.scenarioId) ? (
-                <Query query={myQuery}>
-                    {
-                        ({ loading, error, data }) => {
-                            if (loading) return <div>Loading ...</div>
-                            if (error) return <div>Error</div>
-                            
-                            let response = {}
-                            let state = {}
-                            if (this.state.scenarioId === "soartech-september-demo-scenario-1") {
-                                response = data.getAllHistory[0].history[1].response
-                                state = response.state
-                            } else {
-                                state = data.getAllHistory[1].history[1].response
+                {(this.props.selectedScenario && this.state.scenarioId) ? (
+                    <Query query={getScenarioQuery} variables={{ "scenarioId": this.props.selectedScenario }}>
+                        {
+                            ({ loading, error, data }) => {
+                                if (loading) return <div>Loading ...</div>
+                                if (error) return <div>Error</div>
+
+                                const state = data.getScenario.state
+                                const scenario = data.getScenario
+                                
+                                return (
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header>Scenario Details: {scenario.id}: {scenario.name}</Accordion.Header>
+                                        {state.casualties &&
+                                            <Accordion.Body>
+                                                <p><strong>Scenario Description:</strong></p>
+                                                <p>{state.unstructured}</p>
+                                                <p><strong>Number of Patients:</strong> {state.casualties.length}</p>
+                                                <p><strong>Patients: </strong></p>
+                                                <Accordion>
+                                                    {state.casualties.map((patient, index) => (
+                                                        <Accordion.Item key={index} eventKey={index}>
+                                                            <Accordion.Header>{nameMappings[patient.id]}</Accordion.Header>
+                                                            <Accordion.Body>
+                                                                {this.renderPatientInfo(patient)}
+                                                            </Accordion.Body>
+                                                        </Accordion.Item>
+                                                    ))}
+                                                </Accordion>
+                                            </Accordion.Body>
+                                        }
+                                    </Accordion.Item>
+                                )
                             }
-                            console.log(data, state);
-                            
-                            return (
-                                <Accordion.Item eventKey="0">
-                                    <Accordion.Header>Scenario Details: {this.state.scenarioId}</Accordion.Header>
-                                    {state.casualties && 
-                                        <Accordion.Body>
-                                            <p><strong>Scenario Description:</strong></p>
-                                            <p>{state.unstructured}</p>
-                                            <p><strong>Number of Patients:</strong> {state.casualties.length}</p>
-                                            <p><strong>Patients: </strong></p>
-                                            <Accordion>
-                                                {state.casualties.map((patient, index) => (
-                                                    <Accordion.Item key={index} eventKey={index}>
-                                                        <Accordion.Header>{nameMappings[patient.id]}</Accordion.Header>
-                                                        <Accordion.Body>
-                                                            {this.renderPatientInfo(patient)}
-                                                        </Accordion.Body>
-                                                    </Accordion.Item>
-                                                ))}
-                                            </Accordion>
-                                        </Accordion.Body>
-                                    }
-                                </Accordion.Item>
-                            )
                         }
-                    }
-                </Query>
+                    </Query>
                 ) : (
                     <Accordion.Header>Scenario Details: {this.state.scenarioId}</Accordion.Header>
                 )
