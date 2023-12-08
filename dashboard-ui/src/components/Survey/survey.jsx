7400bfb19f5384ca17fc71bdf58c0e497e9c05bf
dereknop <dereknop@gmail.com>
2023-12-08 16:55:01 -0500

    upload results to mongodb

--- dashboard-ui/src/components/Survey/survey.jsx
+++ dashboard-ui/src/components/Survey/survey.jsx
@@ -3,11 +3,23 @@ import 'survey-core/defaultV2.min.css'
 import { Model } from 'survey-core'
 import { Survey } from "survey-react-ui"
 import surveyConfig from './surveyConfig.json';
+import gql from "graphql-tag";
+import { Mutation } from '@apollo/react-components';
+
+
+const UPLOAD_SURVEY_RESULTS = gql`
+    mutation UploadSurveyResults($results: JSON) {
+        uploadSurveyResults(results: $results)
+    }`;
 
 class SurveyPage extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            uploadData: false
+        };
 
         this.survey = new Model(surveyConfig);
         this.questionStartTimes = {};
@@ -26,7 +38,7 @@ class SurveyPage extends React.Component {
             const startTime = this.questionStartTimes[previousQuestionName];
             const timeSpentInSeconds = (endTime - startTime) / 1000;
 
-            // Update time spent for the previous question
+            // update time spent for the previous question
             this.surveyData[previousQuestionName] = {}
             this.surveyData[previousQuestionName].timeSpentOnQuestion = timeSpentInSeconds;
         }
@@ -35,32 +47,28 @@ class SurveyPage extends React.Component {
     }
 
     onSurveyComplete = (survey) => {
-        // Capture the completion time for the last question
+        // capture the completion time for the last question
         const lastQuestionName = Object.keys(this.questionStartTimes).pop();
         const endTime = new Date();
         const startTime = this.questionStartTimes[lastQuestionName];
         const timeSpentInSeconds = (endTime - startTime) / 1000;
 
-        // Update time spent for the last question
+        // update time spent for the last question
         this.surveyData[lastQuestionName] = {};
         this.surveyData[lastQuestionName].timeSpentOnQuestion = timeSpentInSeconds;
 
-        console.log(survey.valuesHash);
-        const surveyData = this.surveyData;
-        surveyData.user = this.props.currentUser;
+        this.surveyData.user = this.props.currentUser;
 
         // Iterate through each question in survey.valuesHash
         for (const questionName in survey.valuesHash) {
             if (survey.valuesHash.hasOwnProperty(questionName)) {
                 const questionValue = survey.valuesHash[questionName];
 
-                // Check if there is time data for this question in this.surveyData
-                if (surveyData.hasOwnProperty(questionName)) {
-                    // Combine the survey data with the question value
-                    surveyData[questionName].response = questionValue;
+                if (this.surveyData.hasOwnProperty(questionName)) {
+                    // combine the survey data with the question value
+                    this.surveyData[questionName].response = questionValue;
                 } else {
-                    // If there is no time data, create a new entry with the question value
-                    surveyData[questionName] = { response: questionValue };
+                    this.surveyData[questionName] = { response: questionValue };
                 }
             }
         }
@@ -68,21 +76,40 @@ class SurveyPage extends React.Component {
         let shouldDownload = window.confirm("Would you like to download the survey results?")
 
         if (shouldDownload) {
-            const jsonString = JSON.stringify(surveyData, null, 2);
+            this.surveyData = JSON.stringify(this.surveyData, null, 2);
 
             const downloadLink = document.createElement('a');
-            downloadLink.href = URL.createObjectURL(new Blob([jsonString], { type: 'application/json' }));
+            downloadLink.href = URL.createObjectURL(new Blob([this.surveyData], { type: 'application/json' }));
             downloadLink.download = 'survey_results.json';
 
             document.body.appendChild(downloadLink);
             downloadLink.click();
             document.body.removeChild(downloadLink);
+            
+            this.setState({uploadData: true})
         }
     }
 
     render() {
         return (
-            <Survey model={this.survey} />
+            <>
+                <Survey model={this.survey} />
+                {this.state.uploadData && (
+                    <Mutation mutation={UPLOAD_SURVEY_RESULTS}>
+                        {(uploadSurveyResults, { data }) => (
+                            <div>
+                                <button onClick = {(e) => {
+                                    e.preventDefault();
+                                    uploadSurveyResults({
+                                        variables: { results: JSON.parse(this.surveyData)}
+                                    })
+                                }}>Upload Results</button>
+                            </div>
+                            )}
+                    </Mutation>
+                )
+                }
+            </>
         )
     }
 }
