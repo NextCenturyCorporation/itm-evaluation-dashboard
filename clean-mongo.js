8531ba82ea7c61314d669a8e38587542aca00f10
dereknop <dereknop@gmail.com>
2024-02-14 18:45:49 -0500

    script for cleaning junk responses from mongo

---
+++ clean-mongo.js
@@ -0,0 +1,45 @@
+// emails of people who have populated test data
+const query = {
+    "$or": [
+        {
+            // newer format email addresses
+            "results.user.emails.address": {
+                $in: [
+                    "dereknop@gmail.com",
+                    "jennifer.mcvay@caci.com",
+                    "ewartdevisser@gmail.com",
+                    "kaitlyn.sharo@caci.com",
+                    "derek.noppinger@caci.com"
+                ]
+            }
+        },
+        {
+            // old survey results (only the orignal version from qualtrics have this field)
+            "Team": { $exists: true }
+        },
+        {
+            // old format email addresses
+            "user.currentUser.emails.address": {
+                $in: [
+                    "dereknop@gmail.com",
+                    "jennifer.mcvay@caci.com",
+                    "ewartdevisser@gmail.com",
+                    "kaitlyn.sharo@caci.com",
+                    "derek.noppinger@caci.com"
+                ]
+            }
+        },
+        { 
+            "Participant ID.questions.Participant ID.response": { 
+                "$regex": /test/i 
+            } 
+        }
+    ]
+};
+
+// select dashboard db
+db = db.getSiblingDB('dashboard')
+
+// delete all matches
+db.surveyResults.deleteMany(query);
+
