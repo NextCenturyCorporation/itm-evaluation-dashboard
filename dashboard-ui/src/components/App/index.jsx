263be761b5e231748aedf6c6f7914693769e171e
dereknop <dereknop@gmail.com>
2024-09-23 12:06:03 -0400

    addressing cascading issues

--- dashboard-ui/src/components/App/index.jsx
+++ dashboard-ui/src/components/App/index.jsx
@@ -43,14 +43,14 @@ const history = createBrowserHistory();
 
 
 const GET_CONFIGS = process.env.REACT_APP_SURVEY_VERSION === '4.0'
-  ? gql`
+    ? gql`
     query GetConfigs {
       getAllSurveyConfigs
       getAllTextBasedConfigs
       getAllTextBasedImages
     }
   `
-  : gql`
+    : gql`
     query GetConfigs {
       getAllSurveyConfigs
       getAllImageUrls
@@ -232,14 +232,17 @@ export class App extends React.Component {
     setupTextBasedConfig(data) {
         if (data && data.getAllTextBasedConfigs) {
             for (const config of data.getAllTextBasedConfigs) {
+                // don't try to match images to old mre text based
                 let tempConfig = JSON.parse(JSON.stringify(config))
-                for (const page of tempConfig.pages) {
-                    for (const el of page.elements) {
-                        if (Object.keys(el).includes("patients")) {
-                            for (const patient of el.patients) {
-                                const foundImg = data.getAllTextBasedImages.find((x) => (x.casualtyId.toLowerCase() === patient.id.toLowerCase() && x.scenarioId === page.scenario_id));
-                                if (foundImg) {
-                                    patient.imgUrl = foundImg.imageByteCode
+                if (tempConfig.eval != 'mre-eval') {
+                    for (const page of tempConfig.pages) {
+                        for (const el of page.elements) {
+                            if (Object.keys(el).includes("patients")) {
+                                for (const patient of el.patients) {
+                                    const foundImg = data.getAllTextBasedImages.find((x) => (x.casualtyId.toLowerCase() === patient.id.toLowerCase() && x.scenarioId === page.scenario_id));
+                                    if (foundImg) {
+                                        patient.imgUrl = foundImg.imageByteCode
+                                    }
                                 }
                             }
                         }
