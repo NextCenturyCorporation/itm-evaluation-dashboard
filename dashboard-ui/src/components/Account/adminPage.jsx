8165d9bc273c2ab67ada0ad7512e3984c17345fd
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2024-12-17 12:04:49 -0500

    started adding additional protection to admin calls

--- dashboard-ui/src/components/Account/adminPage.jsx
+++ dashboard-ui/src/components/Account/adminPage.jsx
@@ -7,6 +7,7 @@ import { Button, Modal, Form, Container, Row, Col, Card, Spinner } from 'react-b
 import { useSelector } from "react-redux";
 import '../../css/admin-page.css';
 import { setSurveyVersion, setupConfigWithImages } from '../App/setupUtils';
+import { accountsPassword } from '../../services/accountsService';
 
 const getUsersQueryName = "getUsers";
 const GET_USERS = gql`
@@ -28,8 +29,8 @@ const UPDATE_EVAL_IDS_BYPAGE = gql`
 `;
 
 const UPDATE_ADMIN_USER = gql`
-    mutation updateAdminUser($username: String!, $isAdmin: Boolean!) {
-        updateAdminUser(username: $username, isAdmin: $isAdmin) 
+    mutation updateAdminUser($caller: JSON!, $username: String!, $isAdmin: Boolean!) {
+        updateAdminUser(caller: $caller, username: $username, isAdmin: $isAdmin) 
     }
 `;
 
@@ -63,24 +64,20 @@ const UPDATE_SURVEY_VERSION = gql`
     }
 `;
 
-function InputBox({ options, selectedOptions, mutation, param, header }) {
+function InputBox({ options, selectedOptions, mutation, param, header, caller }) {
     const [selected, setSelected] = useState(selectedOptions.sort());
     const [updateUserCall] = useMutation(mutation);
     const updateUser = (newSelect) => {
-        for (let i = 0; i < newSelect.length; i++) {
-            const vars = { variables: { username: newSelect[i] } };
-            if (!selected.includes(newSelect[i])) {
-                vars['variables'][param] = true;
-                updateUserCall(vars);
+        const usersToAdd = newSelect.filter(user => !selected.includes(user));
+        const usersToRemove = selected.filter(user => !newSelect.includes(user));
+        [...usersToAdd, ...usersToRemove].map(username => updateUserCall({
+            variables: {
+                username,
+                [param]: usersToAdd.includes(username) ? true : false,
+                caller
             }
-        }
-        for (let i = 0; i < selected.length; i++) {
-            const vars = { variables: { username: selected[i] } };
-            if (!newSelect.includes(selected[i])) {
-                vars['variables'][param] = false;
-                updateUserCall(vars);
-            }
-        }
+        }));
+
         setSelected(newSelect);
     }
     options.sort((a, b) => (a.value > b.value) ? 1 : -1);
@@ -190,6 +187,10 @@ function AdminPage({ currentUser }) {
     const surveyConfigs = useSelector(state => state.configs.surveyConfigs);
     const [surveyVersions, setSurveyVersions] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [confirmedAdmin, setConfirmedAdmin] = useState(false);
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [sessionId, setSessionId] = useState(null);
 
     const { loading: surveyVersionLoading, error: surveyVersionError, data: surveyVersionData } = useQuery(GET_CURRENT_SURVEY_VERSION, {
         fetchPolicy: 'no-cache'
@@ -275,6 +276,29 @@ function AdminPage({ currentUser }) {
         setShowConfirmation(false);
     };
 
+    const onChangePassword = (event) => {
+        setPasswordError('');
+        setPassword(event.target.value);
+    };
+
+    const verifyIdentity = async (event) => {
+        event.preventDefault();
+        try {
+            let results;
+            results = await accountsPassword.login({
+                password: password,
+                user: {
+                    username: currentUser.username,
+                }
+            });
+            setSessionId(results.sessionId);
+            setConfirmedAdmin(true);
+        } catch (err) {
+            setConfirmedAdmin(false);
+            setPasswordError(err.message);
+        }
+    };
+
     if (surveyVersionLoading) return <div className="loading">Loading survey version...</div>;
     if (surveyVersionError) return <div className="error">Error loading survey version: {surveyVersionError.message}</div>;
 
@@ -304,7 +328,21 @@ function AdminPage({ currentUser }) {
                 </Col>
             </Row>
 
-            <Row className="mb-4">
+            {!confirmedAdmin && <Card className="login-modal">
+                <h3>Please confirm your identity before continuing.</h3>
+                <form onSubmit={verifyIdentity}>
+                    <div className="form-group">
+                        <div className="input-login-header">Username: {currentUser.username}</div>
+                    </div>
+                    <div className="form-group">
+                        <input placeholder="Enter Password" type="password" id="password" value={password} onChange={onChangePassword} />
+                    </div>
+                    {passwordError != '' && <p className='error-message'>{passwordError}</p>}
+                    <Button type='submit'>Submit</Button>
+                </form>
+            </Card>}
+
+            {confirmedAdmin && <><Row className="mb-4">
                 <Col md={6}>
                     <Card>
                         <Card.Header as="h5">Survey Version</Card.Header>
@@ -331,81 +369,82 @@ function AdminPage({ currentUser }) {
                 </Col>
             </Row>
 
-            <ConfirmationDialog
-                show={showConfirmation}
-                onConfirm={confirmSurveyVersionChange}
-                onCancel={cancelSurveyVersionChange}
-                message={`Are you sure you want to change to Survey Version ${pendingSurveyVersion}? This action may affect ongoing surveys.`}
-            />
+                <ConfirmationDialog
+                    show={showConfirmation}
+                    onConfirm={confirmSurveyVersionChange}
+                    onCancel={cancelSurveyVersionChange}
+                    message={`Are you sure you want to change to Survey Version ${pendingSurveyVersion}? This action may affect ongoing surveys.`}
+                />
 
-            <Query query={GET_USERS} fetchPolicy={'no-cache'}>
-                {({ loading, error, data }) => {
-                    if (loading) return <div className="loading">Loading ...</div>;
-                    if (error) return <div className="error">Error: {error.message}</div>;
+                <Query query={GET_USERS} fetchPolicy={'no-cache'}>
+                    {({ loading, error, data }) => {
+                        if (loading) return <div className="loading">Loading ...</div>;
+                        if (error) return <div className="error">Error: {error.message}</div>;
 
-                    const nonSelected = { 'admin': [], 'evaluators': [], 'experimenters': [], 'adept': [] };
+                        const nonSelected = { 'admin': [], 'evaluators': [], 'experimenters': [], 'adept': [] };
 
-                    let adminSelectedOptions = [];
-                    let evaluatorSelectedOptions = [];
-                    let experimenterSelectedOptions = [];
-                    let adeptSelectedOptions = [];
+                        let adminSelectedOptions = [];
+                        let evaluatorSelectedOptions = [];
+                        let experimenterSelectedOptions = [];
+                        let adeptSelectedOptions = [];
 
-                    const users = data[getUsersQueryName]
-                    for (let i = 0; i < users.length; i++) {
-                        for (let k in nonSelected) {
-                            nonSelected[k].push({ value: users[i].username, label: users[i].username + " (" + (users[i].emails !== undefined ? users[i].emails[0].address : "") + ")" });
+                        const users = data[getUsersQueryName]
+                        for (let i = 0; i < users.length; i++) {
+                            for (let k in nonSelected) {
+                                nonSelected[k].push({ value: users[i].username, label: users[i].username + " (" + (users[i].emails !== undefined ? users[i].emails[0].address : "") + ")" });
+                            }
+                            if (users[i].admin) {
+                                adminSelectedOptions.push(users[i].username);
+                            }
+
+                            if (users[i].evaluator) {
+                                evaluatorSelectedOptions.push(users[i].username);
+                            }
+
+                            if (users[i].experimenter) {
+                                experimenterSelectedOptions.push(users[i].username);
+                            }
+
+                            if (users[i].adeptUser) {
+                                adeptSelectedOptions.push(users[i].username);
+                            }
                         }
-                        if (users[i].admin) {
-                            adminSelectedOptions.push(users[i].username);
-                        }
 
-                        if (users[i].evaluator) {
-                            evaluatorSelectedOptions.push(users[i].username);
-                        }
+                        return (
+                            <>
+                                <InputBox options={nonSelected['admin']} selectedOptions={adminSelectedOptions} mutation={UPDATE_ADMIN_USER} param={'isAdmin'} header={'Administrators'} caller={{ username: currentUser.username, sessionId }} />
+                                <InputBox options={nonSelected['evaluators']} selectedOptions={evaluatorSelectedOptions} mutation={UPDATE_EVALUATOR_USER} param={'isEvaluator'} header={'Evaluators'} caller={{ username: currentUser.username, sessionId }} />
+                                <InputBox options={nonSelected['experimenters']} selectedOptions={experimenterSelectedOptions} mutation={UPDATE_EXPERIMENTER_USER} param={'isExperimenter'} header={'Experimenters'} caller={{ username: currentUser.username, sessionId }} />
+                                <InputBox options={nonSelected['adept']} selectedOptions={adeptSelectedOptions} mutation={UPDATE_ADEPT_USER} param={'isAdeptUser'} header={'ADEPT Users'} caller={{ username: currentUser.username, sessionId }} />
+                            </>
+                        );
+                    }}
+                </Query>
 
-                        if (users[i].experimenter) {
-                            experimenterSelectedOptions.push(users[i].username);
-                        }
+                <Query query={GET_EVAL_IDS} fetchPolicy={'no-cache'}>
+                    {({ loading, error, data }) => {
+                        if (loading) return <div className="loading">Loading ...</div>;
+                        if (error) return <div className="error">Error: {error.message}</div>;
 
-                        if (users[i].adeptUser) {
-                            adeptSelectedOptions.push(users[i].username);
-                        }
-                    }
-
-                    return (
-                        <>
-                            <InputBox options={nonSelected['admin']} selectedOptions={adminSelectedOptions} mutation={UPDATE_ADMIN_USER} param={'isAdmin'} header={'Administrators'} />
-                            <InputBox options={nonSelected['evaluators']} selectedOptions={evaluatorSelectedOptions} mutation={UPDATE_EVALUATOR_USER} param={'isEvaluator'} header={'Evaluators'} />
-                            <InputBox options={nonSelected['experimenters']} selectedOptions={experimenterSelectedOptions} mutation={UPDATE_EXPERIMENTER_USER} param={'isExperimenter'} header={'Experimenters'} />
-                            <InputBox options={nonSelected['adept']} selectedOptions={adeptSelectedOptions} mutation={UPDATE_ADEPT_USER} param={'isAdeptUser'} header={'ADEPT Users'} />
-                        </>
-                    );
-                }}
-            </Query>
-
-            <Query query={GET_EVAL_IDS} fetchPolicy={'no-cache'}>
-                {({ loading, error, data }) => {
-                    if (loading) return <div className="loading">Loading ...</div>;
-                    if (error) return <div className="error">Error: {error.message}</div>;
-
-                    return (
-                        <Row>
-                            <Col>
-                                <Card>
-                                    <Card.Header as="h5">Visible Evaluation Options By Page</Card.Header>
-                                    <Card.Body>
-                                        <Card.Text>
-                                            Control values populated in the Evaluation Dropdown list
-                                        </Card.Text>
-                                        <h6>Modify Visible Evaluation Options</h6>
-                                        <EvaluationIDSTable data={data["getEvalIds"]} />
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                    );
-                }}
-            </Query>
+                        return (
+                            <Row>
+                                <Col>
+                                    <Card>
+                                        <Card.Header as="h5">Visible Evaluation Options By Page</Card.Header>
+                                        <Card.Body>
+                                            <Card.Text>
+                                                Control values populated in the Evaluation Dropdown list
+                                            </Card.Text>
+                                            <h6>Modify Visible Evaluation Options</h6>
+                                            <EvaluationIDSTable data={data["getEvalIds"]} />
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        );
+                    }}
+                </Query>
+            </>}
         </Container>
     );
 }
