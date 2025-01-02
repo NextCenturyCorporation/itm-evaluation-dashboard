ab21364d41348f458be2a2424cd5c13ff9f31ac7
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2025-01-02 15:15:21 -0500

    added text route again

--- dashboard-ui/src/components/App/index.jsx
+++ dashboard-ui/src/components/App/index.jsx
@@ -571,12 +571,16 @@ export class App extends React.Component {
                                                     <Route path="/login">
                                                         <Login newState={this.state} userLoginHandler={this.userLoginHandler} participantLoginHandler={this.participantLoginHandler} testerLogin={false} logout={this.logout} />
                                                     </Route>
+
                                                     <Route path="/participantText">
                                                         <Login newState={this.state} userLoginHandler={this.userLoginHandler} participantLoginHandler={this.participantLoginHandler} participantTextLogin={true} testerLogin={false} />
                                                     </Route>
                                                     <Route path="/reset-password/:token" component={ResetPassPage} />
                                                     <Route path="/remote-text-survey">
                                                         <StartOnline />
+                                                    </Route>
+                                                    <Route path="/text-based">
+                                                        <TextBased />
                                                     </Route>
                                                     <Route path="/myaccount">
                                                         <MyAccount newState={this.state} userLoginHandler={this.userLoginHandler} />
