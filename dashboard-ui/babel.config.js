c00d37a78c06950b151dd1572ab8d6ab0b6bb127
kaitlyn.sharo <kaitlyn.sharo@caci.com>
2025-01-24 11:21:08 -0500

    fixed babel import error

--- dashboard-ui/babel.config.js
+++ dashboard-ui/babel.config.js
@@ -3,7 +3,5 @@ module.exports = {
         '@babel/preset-env',     // For modern JavaScript features like async/await
         '@babel/preset-react',   // For transforming JSX syntax (React)
     ],
-    plugins: [
-        '@babel/plugin-syntax-dynamic-import',  // For handling dynamic imports (optional)
-    ],
+    plugins: ["@babel/plugin-syntax-dynamic-import"]
 };
