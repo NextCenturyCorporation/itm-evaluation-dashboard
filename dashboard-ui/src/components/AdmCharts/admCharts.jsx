e6cbb1297bf95a2c2e5994e56bf35b0d0703273d
dereknop <dereknop@gmail.com>
2025-03-26 11:57:27 -0400

    remove eval 7 from adm-results

--- dashboard-ui/src/components/AdmCharts/admCharts.jsx
+++ dashboard-ui/src/components/AdmCharts/admCharts.jsx
@@ -15,16 +15,18 @@ class AdmCharts extends React.Component {
 
     constructor(props) {
         super();
-        props.evaluationOptions.sort((a, b) => (a.value < b.value) ? 1 : -1)
 
+        const filteredOptions = props.evaluationOptions.filter(option => option.value !== 7);
+        filteredOptions.sort((a, b) => (a.value < b.value) ? 1 : -1);
+        this.filteredOptions = filteredOptions
         this.state = {
-            currentEval: props.evaluationOptions[0],
+            currentEval: filteredOptions[0],
         }
 
         this.selectEvaluation = this.selectEvaluation.bind(this);
     }
 
-    selectEvaluation(target){
+    selectEvaluation(target) {
         this.setState({
             currentEval: target
         });
@@ -32,24 +34,24 @@ class AdmCharts extends React.Component {
 
     getHeaderLabel(id, name) {
         if (this.state.currentEval.value < 3) {
-            if(id.toLowerCase().indexOf("adept") > -1 ) {
+            if (id.toLowerCase().indexOf("adept") > -1) {
                 return ("BBN: " + id + " " + name);
             } else {
                 return ("Soartech: " + id + " " + name);
             }
         } else if (this.state.currentEval.value == 3) {
-            if(id.toLowerCase().indexOf("metricseval") > -1 ) {
+            if (id.toLowerCase().indexOf("metricseval") > -1) {
                 return ("ADEPT: " + id + " " + name);
             } else {
                 return ("Soartech: " + name);
             }
         } else {
-            if(id.toLowerCase().includes("qol") || id.toLowerCase().includes("vol") ) {
+            if (id.toLowerCase().includes("qol") || id.toLowerCase().includes("vol")) {
                 return ("Soartech: " + name + " (" + id + ")");
             } else {
                 return ("Adept: " + name + " (" + id + ")");
             }
-        
+
         }
     }
 
@@ -62,7 +64,7 @@ class AdmCharts extends React.Component {
                         <div className="evaluation-selector-holder">
                             <Select
                                 onChange={this.selectEvaluation}
-                                options={this.props.evaluationOptions}
+                                options={this.filteredOptions}
                                 defaultValue={this.state.currentEval}
                             />
                         </div>
@@ -70,43 +72,43 @@ class AdmCharts extends React.Component {
                 </div>
                 {this.state.currentEval !== '' &&
                     <Query query={scenario_names_aggregation} variables={{ "evalNumber": this.state?.currentEval?.value }}>
-                    {
-                        ({ loading, error, data }) => {
-                            if (loading) return <div>Loading ...</div> 
-                            if (error) return <div>Error</div>
+                        {
+                            ({ loading, error, data }) => {
+                                if (loading) return <div>Loading ...</div>
+                                if (error) return <div>Error</div>
 
-                            const scenarioNameOptions = data[getScenarioNamesQueryName];
-                            let scenariosArray = [];
-                            for(let i=0; i < scenarioNameOptions.length; i++) {
-                                scenariosArray.push({
-                                    "id": scenarioNameOptions[i]._id.id, 
-                                    "name": scenarioNameOptions[i]._id.name
-                                });
-                            }
-                            scenariosArray.sort((a, b) => (a.id > b.id) ? 1 : -1);
+                                const scenarioNameOptions = data[getScenarioNamesQueryName];
+                                let scenariosArray = [];
+                                for (let i = 0; i < scenarioNameOptions.length; i++) {
+                                    scenariosArray.push({
+                                        "id": scenarioNameOptions[i]._id.id,
+                                        "name": scenarioNameOptions[i]._id.name
+                                    });
+                                }
+                                scenariosArray.sort((a, b) => (a.id > b.id) ? 1 : -1);
 
-                            return (
-                                <div className="home-container">
-                                    {
-                                        scenariosArray.map(scenario =>
-                                            <div className='chart-home-container' key={"id_" +scenario.id}>
-                                                <div className='chart-header'>
-                                                    <div className='chart-header-label'>
-                                                        <h4>{this.getHeaderLabel(scenario.id, scenario.name)}</h4>
+                                return (
+                                    <div className="home-container">
+                                        {
+                                            scenariosArray.map(scenario =>
+                                                <div className='chart-home-container' key={"id_" + scenario.id}>
+                                                    <div className='chart-header'>
+                                                        <div className='chart-header-label'>
+                                                            <h4>{this.getHeaderLabel(scenario.id, scenario.name)}</h4>
+                                                        </div>
                                                     </div>
+                                                    <ScoreChart testid={scenario.id} evalNumber={this.state.currentEval.value} />
                                                 </div>
-                                                <ScoreChart testid={scenario.id} evalNumber={this.state.currentEval.value}/>
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            )
+                                            )
+                                        }
+                                    </div>
+                                )
+                            }
                         }
-                    }
                     </Query>
                 }
             </div>
-            
+
         );
     }
 }
