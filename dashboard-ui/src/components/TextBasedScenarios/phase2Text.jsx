6a71a359657888e4a50190767486697ec3493b7a
dereknop <dereknop@gmail.com>
2025-05-29 12:33:52 -0400

    phase2text styling

--- dashboard-ui/src/components/TextBasedScenarios/phase2Text.jsx
+++ dashboard-ui/src/components/TextBasedScenarios/phase2Text.jsx
@@ -1,59 +1,57 @@
 import React from "react";
 import { ElementFactory, Question, Serializer } from "survey-core";
 import { SurveyQuestionElementBase } from "survey-react-ui";
-import '../../css/medical-scenario.css';
-import store from '../../store/store';
-
+import '../../css/phase2Text.css';
 
 const CUSTOM_TYPE = "phase2Text";
 
 export class Phase2TextModel extends Question {
-  getType() {
-    return CUSTOM_TYPE;
-  }
+    getType() {
+        return CUSTOM_TYPE;
+    }
 
-  get unstructured() {
-    return this.getPropertyValue("unstructured");
-  }
+    get unstructured() {
+        return this.getPropertyValue("unstructured");
+    }
 
-  set unstructured(unstructured) {
-    this.setPropertyValue("unstructured", unstructured);
-  }
+    set unstructured(unstructured) {
+        this.setPropertyValue("unstructured", unstructured);
+    }
 }
 
 Serializer.addClass(
-  CUSTOM_TYPE,
-  [
-    { name: "unstructured", default: '' }
-  ],
-  function () {
-    return new Phase2TextModel("");
-  },
-  "question"
+    CUSTOM_TYPE,
+    [
+        { name: "unstructured", default: '' }
+    ],
+    function () {
+        return new Phase2TextModel("");
+    },
+    "question"
 );
 
 ElementFactory.Instance.registerElement(CUSTOM_TYPE, (name) => {
-  return new Phase2TextModel(name);
+    return new Phase2TextModel(name);
 });
 
 export class Phase2Text extends SurveyQuestionElementBase {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  get question() {
-    return this.questionBase;
-  }
+    get question() {
+        return this.questionBase;
+    }
 
-  get unstructured() {
-    return this.question.unstructured;
-  }
+    get unstructured() {
+        return this.question.unstructured;
+    }
 
-
-  renderElement() {
-
-    return (
-        <p>{this.unstructured}</p>
-    );
-  }
+    renderElement() {
+        return (
+            <div className="phase2-text-container">
+                <p className="phase2-text-content">{this.unstructured}</p>
+            </div>
+        );
+    }
 }
