aa35c6f0636ada6c5f3645abba499f47a47ab546
dereknop <dereknop@gmail.com>
2025-06-19 12:18:16 -0400

    consent form

--- dashboard-ui/src/components/OnlineOnly/OnlineOnly.jsx
+++ dashboard-ui/src/components/OnlineOnly/OnlineOnly.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useMutation, useQuery } from '@apollo/react-hooks';
+import { Modal, Button } from 'react-bootstrap';
+import consentPdf from './consentForm2025.pdf';
 import gql from "graphql-tag";
 import { TextBasedScenariosPageWrapper } from "../TextBasedScenarios/TextBasedScenariosPage";
 import { useHistory, useLocation } from 'react-router-dom';
@@ -46,23 +48,37 @@ const LOG_VARIATIONS = [
 export default function StartOnline() {
     const { refetch } = useQuery(GET_PARTICIPANT_LOG, { fetchPolicy: 'no-cache' });
     const [addParticipant] = useMutation(ADD_PARTICIPANT);
+    const [showConsentForm, setShowConsentForm] = React.useState(false);
     const [textTime, setTextTime] = React.useState(false);
     const history = useHistory();
     const location = useLocation();
 
 
     React.useEffect(() => {
-        // make sure adeptQualtrix is true, otherwise send to login
         const queryParams = new URLSearchParams(window.location.search);
         const adeptQualtrix = queryParams.get('adeptQualtrix');
-        const caciProlific = queryParams.get('caciProlific')
-        if (adeptQualtrix != 'true' && caciProlific != 'true') {
+        const caciProlific = queryParams.get('caciProlific');
+
+        if (adeptQualtrix === 'true') {
+            // continue to instructions or auto-start
+            if (queryParams.get('startSurvey') === 'true') setTextTime(true);
+        } else if (caciProlific === 'true') {
+            setShowConsentForm(true);
+        } else {
             history.push('/login');
         }
-        if (queryParams.get('startSurvey') == 'true') {
-            setTextTime(true);
-        }
     }, []);
+
+    const handleConsentResponse = (agree) => {
+        if (!agree) {
+            window.location.href = caciReturnURL;
+        } else {
+            setShowConsentForm(false);
+            const queryParams = new URLSearchParams(window.location.search);
+            if (queryParams.get('startSurvey') === 'true') setTextTime(true);
+        }
+    };
+
 
     const startSurvey = async () => {
         // get pid
@@ -99,24 +115,56 @@ export default function StartOnline() {
         setTextTime(true);
     }
 
+    const ConsentForm = ({ show, onAgree, onDisagree }) => (
+        <Modal show={show} backdrop="static" keyboard={false} size="lg" centered>
+            <Modal.Header>
+                <Modal.Title>Consent Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body style={{ height: '70vh', padding: 0 }}>
+                <iframe
+                    src={consentPdf}
+                    title="Consent Form PDF"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 'none' }}
+                />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={onDisagree}>I Do Not Agree</Button>
+                <Button variant="primary" onClick={onAgree}>I Agree</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+
+
+    const caciReturnURL = 'https://app.prolific.com/submissions/complete?cc=C155IMPM'
+
     return (
         <>
-            {!textTime && <div className="text-instructions">
-                <h2>Instructions</h2>
-                <p><b>Welcome to the ITM Text Scenario experiment. Thank you for your participation.</b>
-                    <br />
-                    During this portion of the experiment, you will be presented with several medical triage scenarios. You will be given action options from which to choose. Please consider
-                    how you would act if you were placed in this scenario.
-                </p>
-                <h4>Guidelines:</h4>
-                <ul>
-                    <li>Choose the option that best matches how you would triage the scenario.</li>
-                    <li>Read all details to clearly understand each question before responding.</li>
-                </ul>
-                <p className='center-text'>Press "Start" to begin.</p>
-                <button onClick={startSurvey}>Start</button>
-            </div>}
-            {textTime && <TextBasedScenariosPageWrapper />}
+            {showConsentForm ? (
+                <ConsentForm
+                    show={showConsentForm}
+                    onAgree={() => handleConsentResponse(true)}
+                    onDisagree={() => handleConsentResponse(false)}
+                />
+            ) :
+                !textTime ? (<div className="text-instructions">
+                    <h2>Instructions</h2>
+                    <p><b>Welcome to the ITM Text Scenario experiment. Thank you for your participation.</b>
+                        <br />
+                        During this portion of the experiment, you will be presented with several medical triage scenarios. You will be given action options from which to choose. Please consider
+                        how you would act if you were placed in this scenario.
+                    </p>
+                    <h4>Guidelines:</h4>
+                    <ul>
+                        <li>Choose the option that best matches how you would triage the scenario.</li>
+                        <li>Read all details to clearly understand each question before responding.</li>
+                    </ul>
+                    <p className='center-text'>Press "Start" to begin.</p>
+                    <button onClick={startSurvey}>Start</button>
+                </div>) : (
+                    <TextBasedScenariosPageWrapper />
+                )}
         </>
     );
 }
