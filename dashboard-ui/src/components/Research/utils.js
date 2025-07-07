8369ace4d1bd5ba418116fa4c656e4343f949d63
dereknop <dereknop@gmail.com>
2025-07-07 12:55:29 -0400

    baseline doesnt have that target field anymore. generalized approach

--- dashboard-ui/src/components/Research/utils.js
+++ dashboard-ui/src/components/Research/utils.js
@@ -396,7 +396,7 @@ export function getRQ134Data(evalNum, dataSurveyResults, dataParticipantLog, dat
                 if (evalNum < 8) {
                     comparison_entry = comparisons?.find((x) => x['ph1_server'] !== true && x['dre_server'] !== true && x['adm_type'] == t && x['pid'] == pid && getDelEnvMapping(res.results.surveyVersion)[entryObj['Scenario']].includes(x['adm_scenario']) && ((entry['TA2'] == 'Parallax' && x['adm_author'] == 'TAD') || (entry['TA2'] == 'Kitware' && x['adm_author'] == 'kitware')) && x['adm_scenario']?.toLowerCase().includes(entryObj['Attribute']?.toLowerCase()));
                 } else {
-                    comparison_entry = comparisons?.find((x) => x['adm_type'] === t && x['pid'] === pid && x['adm_scenario'] === page['scenarioIndex'] && x['adm_alignment_target'] === entryObj['Target']);
+                    comparison_entry = comparisons?.find((x) => x['adm_type'] === t && x['pid'] === pid && x['adm_scenario'] === page['scenarioIndex'] && x['adm_alignment_target'] === page['admTarget']);
                 }
                 const alignmentComparison = comparison_entry?.score ?? '-'
 
