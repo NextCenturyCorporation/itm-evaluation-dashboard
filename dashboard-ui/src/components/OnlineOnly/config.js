df03529de09a00d7ca918483c29b619fbde018e7
dereknop <dereknop@gmail.com>
2025-08-26 10:46:06 -0400

    sept text loading

--- dashboard-ui/src/components/OnlineOnly/config.js
+++ dashboard-ui/src/components/OnlineOnly/config.js
@@ -67,7 +67,7 @@ export const phase1ParticipantData = (currentSearchParams, hashedEmail, newPid, 
     };
 }
 
-export const phase2ParticipantData = (currentSearchParams, hashedEmail, newPid, type, evalNum) => {
+export const juneJulyParticipantData = (currentSearchParams, hashedEmail, newPid, type, evalNum) => {
     const scenarioSet = Math.floor(Math.random() * 3) + 1;
 
     const prolificId = currentSearchParams ? currentSearchParams.get('PROLIFIC_PID') : null;
@@ -92,29 +92,65 @@ export const phase2ParticipantData = (currentSearchParams, hashedEmail, newPid, 
     };
 };
 
+export const septemberParticipantData = (currentSearchParams, hashedEmail, newPid, type, evalNum) => {
+    const scenarioSet = Math.floor(Math.random() * 3) + 1;
+
+    const prolificId = currentSearchParams ? currentSearchParams.get('PROLIFIC_PID') : null;
+    const contactId = currentSearchParams ? currentSearchParams.get('ContactID') : null;
+    const email = hashedEmail ? hashedEmail : bcrypt.hashSync(newPid.toString(), "$2a$10$" + process.env.REACT_APP_EMAIL_SALT);
+
+    return {
+        "ParticipantID": newPid,
+        "Type": type,
+        "prolificId": prolificId,
+        "contactId": contactId,
+        "claimed": true,
+        "simEntryCount": 0,
+        "surveyEntryCount": 0,
+        "textEntryCount": 0,
+        "hashedEmail": email,
+        "AF-text-scenario": scenarioSet,
+        "MF-text-scenario": scenarioSet,
+        "PS-text-scenario": scenarioSet,
+        "PS-AF-text-scenario": Math.floor(Math.random() * 2) + 1,
+        'evalNum': evalNum
+    };
+}
+
 
 export const scenarioIdsFromLog = (participantLog, currentEval) => {
     const num = evalNameToNumber[currentEval]
     let scenarios = [];
 
-    if (num === 8 || num === 9) {
+    if (num === 10) {
+        const month = 'Sept2025'
+        const scenarioTypes = ['AF', 'MF', 'PS', 'PS-AF']
+
+        scenarios = scenarioTypes.map(type =>
+            `${month}-${type}${participantLog[`${type}-text-scenario`]}-eval`
+        )
+    } else if (num === 8 || num === 9) {
         const monthPrefix = num === 9 ? 'July2025' : 'June2025';
         const scenarioTypes = ['AF', 'MF', 'PS', 'SS'];
 
         scenarios = scenarioTypes.map(type =>
             `${monthPrefix}-${type}${participantLog[`${type}-text-scenario`]}-eval`
         );
-
-        for (let i = scenarios.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [scenarios[i], scenarios[j]] = [scenarios[j], scenarios[i]];
-        }
     } else if (num === 5) {
         scenarios = [
             ...p1Mappings[participantLog['Text-1']] || [],
             ...p1Mappings[participantLog['Text-2']] || []
         ];
     }
+
+    if (num >= 8) {
+        for (let i = scenarios.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [scenarios[i], scenarios[j]] = [scenarios[j], scenarios[i]];
+        }
+    }
+
+    console.log(scenarios)
 
     return scenarios
 }
