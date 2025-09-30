e3ace2dc44f8d879ce98e64a5bbdcdec8a6e211d
Ryan Scott <ryan.scott@caci.com>
2025-09-30 15:30:18 -0400

    puppeteer args

--- dashboard-ui/jest-puppeteer.config.js
+++ dashboard-ui/jest-puppeteer.config.js
@@ -3,6 +3,13 @@ module.exports = {
         headless: true,  // Set to `false` to see the browser window
         // slowMo: 25,       // Optional: Slow down actions to see what is happening
         defaultViewport: null,  // Optional: Fullscreen viewport
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu'
+        ]
     },
     browserContext: 'incognito',
     server: {
