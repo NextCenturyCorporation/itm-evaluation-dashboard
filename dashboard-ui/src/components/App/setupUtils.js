9cc2ef918738e8da458fe101fc87c9638ec53d6f
dereknop <dereknop@gmail.com>
2025-10-15 11:58:56 -0400

    log

--- dashboard-ui/src/components/App/setupUtils.js
+++ dashboard-ui/src/components/App/setupUtils.js
@@ -4,7 +4,6 @@ import { isDefined } from '../AggregateResults/DataFunctions';
 import { setParticipantLog } from '../../store/slices/participantSlice';
 
 export function setupConfigWithImages(data) {
-    console.log(data)
     const hasImageData = data.getAllImageUrls || data.getAllTextBasedImages;
 
     for (const config of data.getAllSurveyConfigs) {
