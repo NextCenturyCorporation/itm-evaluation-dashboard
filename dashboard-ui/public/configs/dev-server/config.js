99a58225b3d8dc253e1a70ef26f193d385ca376f
patrick.mohler <patrick.mohler@caci.com>
2026-01-21 15:55:36 -0500

    Added remaining files for dev-server updates

---
+++ dashboard-ui/public/configs/dev-server/config.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://darpaitm.caci.com/dev/api';
+
+module.exports = {API_URL};
---
+++ dashboard-ui/public/configs/dev-server/graphql-config.js
@@ -0,0 +1,5 @@
+const RESET_PASSWORD_URL = "https://darpaitm.caci.com/dev/reset-password";
+const GRAPHQL_PORT = 9100;
+const MONGO_DB = 'dashboard';
+
+module.exports = {RESET_PASSWORD_URL, GRAPHQL_PORT, MONGO_DB};
