0660294e028338b5bc15cfa8a247cc1f0c6aa8dd
dereknop <dereknop@gmail.com>
2026-04-17 12:24:26 -0400

    remove vr question from demographics online only

--- dashboard-ui/src/components/TextBasedScenarios/TextBasedScenariosPage.jsx
+++ dashboard-ui/src/components/TextBasedScenarios/TextBasedScenariosPage.jsx
@@ -227,6 +227,16 @@ class TextBasedScenariosPage extends Component {
             const configCopy = JSON.parse(JSON.stringify(demographicsConfig));
             configCopy.showTitle = false;
 
+            if (this.state.onlineOnly) {
+                configCopy.pages.forEach(page => {
+                    if (page.elements) {
+                        page.elements = page.elements.filter(el =>
+                            !el.name?.toLowerCase().includes('virtual reality')
+                        );
+                    }
+                });
+            }
+
             const surveyModel = new Model(configCopy);
             surveyModel.applyTheme(surveyTheme);
             surveyModel.onComplete.add(this.demographicsSurveyComplete);
