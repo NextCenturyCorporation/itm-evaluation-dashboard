ccf4d8230bd2afc63c753aecc7439d0f4fb8d49a
Tabernero <kayla.tabernero_cn@cacicorenet.com>
2026-07-15 13:01:25 -0400

    WIP: make error boundary template

---
+++ dashboard-ui/src/components/ErrorHandling/ErrorBoundaries.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { QueryErrorMessage } from "../ErrorHandling/QueryErrorMessage";
+
+export class ErrorBoundary extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasError: false,
+            error: null
+        };
+    }
+
+    static getDerivedStateFromError(error) {
+
+        return {
+            hasError: true,
+            error
+        };
+
+    }
+
+    componentDidCatch(error, errorInfo) {
+
+        console.error("React Error Boundary");
+        console.error(error);
+        console.error(errorInfo);
+
+    }
+
+    render() {
+
+        if (this.state.hasError) {
+
+            return (
+                <QueryErrorMessage
+                    error={this.state.error}
+                />
+            );
+
+        }
+
+        return this.props.children;
+
+    }
+
+}
